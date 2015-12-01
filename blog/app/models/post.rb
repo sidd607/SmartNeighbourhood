7dd54062d662c798499e75c9a33ee8d2d569fb1e
savs95 <apoorvvikram.singh@iiitb.org>
2015-12-01 23:58:14 +0530

    surveys added

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,9 +1,6 @@
-
 class Post < ActiveRecord::Base
   has_many :comments
-  has_many :reports
   belongs_to :profiles
-  acts_as_taggable
   def self.search(search)
     if search
       where('title LIKE ?', "%#{search}%")
